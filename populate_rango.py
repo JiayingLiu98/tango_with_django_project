92a63a1e5f205871100fd076361a34a3d0bf9e8a
JiayingLiu98 <2517510l@student.gla.ac.uk>
2021-07-28 19:17:20 +0800

    Exercise of chapter5 finished

--- populate_rango.py
+++ populate_rango.py
@@ -35,7 +35,7 @@ def populate():
         'Other Framewoeks':{'pages':other_pages,  'views':32, 'likes':16}}
 
     for cat, cat_data in cats.items():
-        c=add_cat(cat)
+        c=add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
