810d84270ffd419ceba916caa36bdf50053a1d69
JiayingLiu98 <2517510l@student.gla.ac.uk>
2021-07-30 11:04:38 +0800

    finished chapter 10 exercises

--- rango/views.py
+++ rango/views.py
@@ -10,6 +10,7 @@ from django.http import HttpResponse
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -23,14 +24,26 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
     #context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-   
+    visitor_cookie_handler(request)
+    #context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'rango/index.html', context=context_dict)
+    
+    return response
    # Return a rendered response to send to the client. 
    # We make use of the shortcut function to make our lives easier
    # Note that the first parameter is the template we wish to use
-    return render(request, 'rango/index.html', context=context_dict)
+   # return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return render(request, 'rango/about.html')
+    context_dict={}
+    visitor_cookie_handler(request)
+    context_dict['visits']=request.session['visits']
+
+    #if request.session.test_cookie_worked():
+       # print("TEST COOKIE WORKED!")
+       # request.session.delete_test_cookie()
+    return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -145,3 +158,23 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,'last_visit',str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+        #response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        request.session['last_visit'] = last_visit_cookie
+        #response.set_cookie('last_visit', last_visit_cookie)
+    request.session['visits'] = visits
+    #response.set_cookie('visits', visits)
